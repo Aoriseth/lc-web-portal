ca6640093990e6cda535c33a0d95f1c82db59c70
Lennart Cockx <lennart.cockx@gmail.com>
2019-08-25 13:15:53 +0200

    Added theming options to application

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import MenuIcon from '@material-ui/icons/Menu';
+import { ThemeProvider } from '@material-ui/styles';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import {orange} from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -23,40 +26,49 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
+const theme = createMuiTheme({
+    palette:{
+        primary:{main:'#345643'},
+        secondary:{main:'#348654'},
+    }
+});
+
+
 
 const App: React.FC = () => {
     const classes = useStyles();
 
   return (
-
-    <div className="App">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-      <div>Test</div>
-        <AppBar><Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-                Main
-            </Typography>
-            <Button color="inherit">Login</Button>
-        </Toolbar></AppBar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+          <div className="App">
+              <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+              <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+              <div>Test</div>
+              <AppBar><Toolbar>
+                  <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                      <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" className={classes.title}>
+                      Main
+                  </Typography>
+                  <Button color="inherit">Login</Button>
+              </Toolbar></AppBar>
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                      Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Learn React
+                  </a>
+              </header>
+          </div>
+      </ThemeProvider>
   );
 };
 
